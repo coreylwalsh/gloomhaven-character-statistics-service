f1e152578095c68c32c0cd3d179c5d9ded2179df
coreylwalsh <110074580+coreylwalsh@users.noreply.github.com>
2023-06-21 17:04:59 -0700

    Add character statistics class

---
+++ src/Domain/Character.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain;
+
+public class Character
+{
+    public Guid CharacterId { get; set; }
+    public string Name { get; set; }
+    public Guid PlayerId { get; set; }
+    public string PlayerName { get; set; }
+    public string PartyName { get; set; }
+    public string Class { get; set; }
+    public int Experience { get; set; }
+    public int Gold { get; set; }
+    public string[] Items { get; set; } = new string[] {}; // change to array of class Item
+    public string[] Perks { get; set; } = new string[] {}; // change to array of class Perk
+    public int Checkmarks { get; set; }
+    public string[] Abilities { get; set; } = new string[] {}; // change to array of class Ability
+    public string PersonalQuest { get; set; }
+    public int LineageRank { get; set; }
+}
